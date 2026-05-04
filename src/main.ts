a6e51cfebb86ab159be99cc2ac85f5668c8dd79f
Michael Schlegel <39999330+Unluckymichell@users.noreply.github.com>
2026-05-04 12:14:57 +0000

    Resetup Calendar every week

--- src/main.ts
+++ src/main.ts
@@ -8,21 +8,7 @@ import { GoogleCalendarManager } from "./GoogleAPI";
 import { promiseBatch } from "./util";
 import { Syncer } from "./Syncer";
 
-async function main() {
-
-  // Pull and verify app config
-  const appENV = new AppENV(process);
-
-  // Login google
-  const googleCalendarManager = new GoogleCalendarManager();
-  if (appENV.GOOGLE_SECRET_JSON) {
-    await googleCalendarManager.authenticateFromJson(appENV.GOOGLE_SECRET_JSON);
-  } else if (appENV.GOOGLE_SECRET_FILE) {
-    await googleCalendarManager.authenticate(appENV.GOOGLE_SECRET_FILE);
-  } else {
-    throw new Error("Missing GOOGLE_SECRET_FILE or GOOGLE_SECRET_JSON");
-  }
-
+async function setupCalendars(googleCalendarManager: GoogleCalendarManager, appENV: AppENV) {
   // Ensure all calendars exist, pulling out calendar_v3 objects
   const userToCalendarMap: { user_pass: string, cal: calendar_v3.Schema$Calendar, emails: string[] }[] = [];
   await promiseBatch(3,
@@ -75,20 +61,50 @@ async function main() {
     digiApi
   ));
 
+  return syncers;
+}
+
+async function main() {
+
+  // Pull and verify app config
+  const appENV = new AppENV(process);
+
+  // Login google
+  const googleCalendarManager = new GoogleCalendarManager();
+  if (appENV.GOOGLE_SECRET_JSON) {
+    await googleCalendarManager.authenticateFromJson(appENV.GOOGLE_SECRET_JSON);
+  } else if (appENV.GOOGLE_SECRET_FILE) {
+    await googleCalendarManager.authenticate(appENV.GOOGLE_SECRET_FILE);
+  } else {
+    throw new Error("Missing GOOGLE_SECRET_FILE or GOOGLE_SECRET_JSON");
+  }
+
+  // First setup of calendars and digiKabu API instances
+  let syncers = await setupCalendars(googleCalendarManager, appENV);
+
   // Run all syncers full sync sequentially
   await promiseBatch(
     1, syncers.map(syncer => () => syncer.syncAll())
   );
   
-  // Run quick syncs every 2 hours from 6am to 6pm Mon-Fri
-  const job1 = schedule.scheduleJob('0/30 6-18 * * 1-5', async () => {
+  // Run weekly resetup job every Sunday at 12:00 to catch any changes in config or calendar setup
+  const job_resetup = schedule.scheduleJob('12 0 * * 0', async () => {
+    console.log("Running weekly resetup job...");
+    // Replace Syncers with newly setup ones
+    syncers = await setupCalendars(googleCalendarManager, appENV);
+  });
+
+  // Run quick syncs every 2 hours from 6am to 6pm Mon-Fri (when changes are most likely to happen)
+  const job_quick_sync = schedule.scheduleJob('0/30 6-18 * * 1-5', async () => {
+    console.log("Running quick sync job...");
     await promiseBatch(
       1, syncers.map(syncer => () => syncer.quickSync())
     );
   });
-  
-  // Run full syncs every night at 1 AM Mon-Fri
-  const job2 = schedule.scheduleJob('0 1 * * 1-5', async () => {
+
+  // Run full syncs every night at 1 AM Mon-Fri to ensure data consistency and catch any missed changes
+  const job_full_sync = schedule.scheduleJob('0 1 * * 1-5', async () => {
+    console.log("Running full sync job...");
     await promiseBatch(
       1, syncers.map(syncer => () => syncer.syncAll())
     );
